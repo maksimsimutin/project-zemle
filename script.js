d798ef9e08e9134de7c0ad338a4dfbfa76ebf574
maksimsimutin <86056653+maksimsimutin@users.noreply.github.com>
2024-03-26 17:55:02 +0200

    26.03

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+
+/*===КНОПКА СДЕЛАТЬ ЗАКАЗ===*/
+
 document.addEventListener('DOMContentLoaded', function() {
     const myBtn = document.getElementById('myBtn');
     const myModal = document.getElementById('myModal');
@@ -11,4 +14,29 @@ document.addEventListener('DOMContentLoaded', function() {
         myModal.classList.remove('is-open');
     });
 });
-  
+ 
+/*===КНОПКА ОТПРАВИТЬ ФОРМУЛУ===*/
+
+document.addEventListener('DOMContentLoaded', function() {
+    const form = document.querySelector('.review-form');
+
+    form.addEventListener('submit', function(event) {
+        event.preventDefault();
+
+        const formData = new FormData(form);
+        const userName = formData.get('user-name');
+        const userTel = formData.get('user-tel');
+        const userEmail = formData.get('user-email');
+        const userComment = formData.get('user-comment');
+
+        console.log(`Name: ${userName}, Tel: ${userTel}, Email: ${userEmail}, Comment: ${userComment}`);
+
+        // Отправка данных на сервер с помощью AJAX-запроса
+        const xhr = new XMLHttpRequest();
+        xhr.open('POST', '/submit-form', true);
+        xhr.send(formData);
+
+        // Очистка полей формы после отправки
+        form.reset();
+    });
+});
